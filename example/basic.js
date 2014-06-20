fd52ed5c784c4adead47ff9982f75df7b184a224
U-DOM01KABA\sergio.trentini <sergio.trentini@KBRW0228.dom01.kaba.grp>
2014-06-20 13:44:54 +0200

    [TENAC-21] updated basic example

--- example/basic.js
+++ example/basic.js
@@ -2,13 +2,12 @@ var module = angular.module('basicExample', []);
 
 module.config(function (CQRSProvider) {
 
-   // tell angular.CQRS
+   // tell angular.CQRS how to build urls for HTTP GET queries
    CQRSProvider.setUrlFactory(function (dataId) {
       return 'http://www.my-backend.com/api/' + dataId;
    });
 
 });
-
 
 module.run(function (CQRS) {
 
@@ -28,14 +27,23 @@ module.run(function (CQRS) {
 
 });
 
-
-module.controller('MainController', function ($scope, Store) {
+module.controller('MainController', function ($scope, Store, CQRS) {
 
    // send a query to the server, requesting data with the id 'name'
    // CQRS will update your scope variable on every update event from the server
-   Store.get('name').then(function (data) {
-      $scope.name = data;
+   Store.get('myProfile').then(function (data) {
+      $scope.profile = data;
    });
 
+   $scope.onClick = function () {
+      CQRS.sendCommand('changeProfile', {
+         id: $scope.profile.id,
+         description: 'newDescription'
+      });
+   };
+
+   $scope.onErase = function () {
+      Store.clear();
+   };
 
 });
