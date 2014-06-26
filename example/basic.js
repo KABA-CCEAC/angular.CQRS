0b57f0691de371d8ef7fdc358daf55b5bb7c06ce
xeronimus <xeronimus@gmail.com>
2014-06-26 17:17:49 +0200

    [TENAC-21] update basic example to match changed API

--- example/basic.js
+++ example/basic.js
@@ -2,52 +2,55 @@ var module = angular.module('basicExample', ['ngCQRS']);
 
 module.config(function (CQRSProvider) {
 
-   // tell angular.CQRS how to build urls for HTTP GET queries
-   CQRSProvider.setUrlFactory(function (dataId) {
-      return 'http://www.my-backend.com/api/' + dataId;
-   });
+  // tell angular.CQRS how to build urls for HTTP GET queries
+  CQRSProvider.setUrlFactory(function (viewModelName, parameters) {
+    return 'http://www.my-backend.com/api/' + viewModelName + CQRSProvider.toUrlGETParameterString(parameters);
+  });
 
 });
 
-module.run(function (CQRS) {
+module.run(function (CQRS, DenormalizationService) {
 
-   // connect angular.CQRS to your socket / long polling solution, etc.
+  // connect angular.CQRS to your socket / long polling solution, etc.
 
-   var mySocket = io();
+  var mySocket = io();
 
-   // pass in events from your socket
-   mySocket.on('events', function (data) {
-      CQRS.eventReceived(data);
-   });
+  // pass in events from your socket
+  mySocket.on('events', function (data) {
+    CQRS.eventReceived(data);
+  });
 
-   // pass commands to socket
-   CQRS.onCommand(function (data) {
-      mySocket.emit('commands', data);
-   });
+  // pass commands to socket
+  CQRS.onCommand(function (data) {
+    mySocket.emit('commands', data);
+  });
+
+  // tell angular.CQRS how to denormalize (or merge) profileChanged events on the modelView personDetailView
+  DenormalizationService.registerDenormalizerFunction('profile', 'person', 'moved', function (oldProfile, payload) {
+    if (payload.id === oldProfile.person.id) {
+      oldProfile.person.address = payload.address;
+    }
+
+    return oldProfile;
+  });
 
 });
 
-module.controller('MainController', function ($scope, Store, CQRS) {
+module.controller('MainController', function ($scope, StoreService, CQRS) {
 
-   // send a query to the server, requesting data with the id 'name'
-   // CQRS will update your scope variable on every update event from the server
-   Store.get('myProfile', function (data) {
-      $scope.profile = data;
-   });
+  var store = StoreService.createForController($scope);
 
-   $scope.$watch('profile', function (newProfile) {
-      // do something when updated...
-   });
+  // send a query to the server, requesting data with the id 'name'
+  // angular.CQRS will invoke your callback on every update event from the server
+  store.for('profile').do(function (personDetails) {
+    $scope.personDetails = personDetails;
+  });
 
-   $scope.onClick = function () {
-      CQRS.sendCommand('changeProfile', {
-         id: $scope.profile.id,
-         description: 'newDescription'
-      });
-   };
-
-   $scope.onErase = function () {
-      Store.clear();
-   };
+  $scope.onChangeProfile = function () {
+    CQRS.sendCommand('person', 'move', {
+      id: $scope.personDetails.id,
+      address: 'my entered new address'
+    });
+  };
 
 });
