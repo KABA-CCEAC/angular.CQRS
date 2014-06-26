f1ca6f8f6ca1e57293ad341bd0f15b0401e48c32
xeronimus <xeronimus@gmail.com>
2014-06-26 08:54:12 +0200

    [TENAC-21] added code coverage report

--- karma.conf.js
+++ karma.conf.js
@@ -1,27 +1,37 @@
 module.exports = function (config) {
-   config.set({
-      basePath: './',
-      frameworks: ['mocha', 'expect'],
+  config.set({
+    basePath: './',
+    frameworks: ['mocha', 'expect'],
 
-      files: [
-         'src/bower_components/angular/angular.js',
-         'src/bower_components/angular-mocks/angular-mocks.js',
-         'src/cqrs/module.js',
-         'src/cqrs/**/*.js',
-         'test/testUtils/**/*.js',
-         'test/**/*.js'
-      ],
-      exclude: [
-         'karma.conf.js'
-      ],
-      logLevel: config.LOG_INFO,
-      browsers: ['PhantomJS'],
-      // web server port
-      port: 8081,
+    files: [
+      'src/bower_components/angular/angular.js',
+      'src/bower_components/angular-mocks/angular-mocks.js',
+      'src/cqrs/module.js',
+      'src/cqrs/**/*.js',
+      'test/testUtils/**/*.js',
+      'test/**/*.js'
+    ],
 
-      // cli runner port
-      runnerPort: 9000,
-      reporters: ['progress'],
-      captureTimeout: 7000
-   });
+    exclude: [
+      'karma.conf.js'
+    ],
+
+    preprocessors: {
+      // source files, that you wanna generate coverage for
+      // do not include tests or libraries
+      // (these files will be instrumented by Istanbul)
+      'src/cqrs/*.js': ['coverage']
+    },
+
+    logLevel: config.LOG_INFO,
+
+    browsers: ['PhantomJS'],
+    // web server port
+    port: 8081,
+
+    // cli runner port
+    runnerPort: 9000,
+    reporters: ['progress', 'coverage'],
+    captureTimeout: 7000
+  });
 };
