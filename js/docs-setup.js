044c464fb252081bc653b7eb35fd2b2208fdf43b
xeronimus <xeronimus@gmail.com>
2014-06-26 18:01:19 +0200

    [TENAC-21] commit docs

---
+++ js/docs-setup.js
@@ -0,0 +1,60 @@
+NG_DOCS={
+  "sections": {
+    "api": "API Reference"
+  },
+  "pages": [
+    {
+      "section": "api",
+      "id": "ngCQRS.provider:CQRSProvider",
+      "shortName": "CQRSProvider",
+      "type": "object",
+      "moduleName": "ngCQRS",
+      "shortDescription": "Handles the configuration of the CQRS module.",
+      "keywords": "angular api configuration cqrs factory form function generate generates handles identifier module ngcqrs object parameter parameters pass provider query registers seturlfactory string tourlgetparameterstring url urlfactoryfunction urls viewmodelname"
+    },
+    {
+      "section": "api",
+      "id": "ngCQRS.service:CQRS",
+      "shortName": "CQRS",
+      "type": "service",
+      "moduleName": "ngCQRS",
+      "shortDescription": "Is used to send commands and define the specific channel over which messages will be sent.",
+      "keywords": "aggregatetype angular api application arrived call channel command commandname commands connection cqrs define event eventreceived executed function listener message messages ngcqrs oncommand payload received register registered send sendcommand sends service specific websocket"
+    },
+    {
+      "section": "api",
+      "id": "ngCQRS.service:DenormalizationService",
+      "shortName": "DenormalizationService",
+      "type": "object",
+      "moduleName": "ngCQRS",
+      "shortDescription": "Used to configure denormalizers.",
+      "keywords": "aggregate aggregatetype angular api callback change client combination configure cqrs data dataset delta denormalization denormalizer denormalizerfunction denormalizers event eventname events existing function functions getdenormalizerfunctions identifier incoming merge ngcqrs object optional original pair pass passed payload register registerdenormalizerfunction registered registering returns service specifiv type viewmodelname"
+    },
+    {
+      "section": "api",
+      "id": "ngCQRS.service:Store",
+      "shortName": "Store",
+      "type": "object",
+      "moduleName": "ngCQRS",
+      "shortDescription": "The store allows for querying modelViews and registering for subsequent events on that viewModelName.",
+      "keywords": "allows angular api callback called cqrs denormalized events function handler identifier modelviews ngcqrs object optional parameters pass passed provider query querying register registerdenormalizerfunctions registering response service seturlfactory store subsequent toghether url urlfactory viewmodelname"
+    },
+    {
+      "section": "api",
+      "id": "ngCQRS.service:StoreService",
+      "shortName": "StoreService",
+      "type": "object",
+      "moduleName": "ngCQRS",
+      "shortDescription": "Used to obtain a Store instance.",
+      "keywords": "$scope angular api clean-up controller correctly createforcontroller createforservice creates destroyed function instance ngcqrs object scope service store"
+    }
+  ],
+  "apis": {
+    "api": true
+  },
+  "html5Mode": false,
+  "startPage": "/api",
+  "scripts": [
+    "angular.min.js"
+  ]
+};
