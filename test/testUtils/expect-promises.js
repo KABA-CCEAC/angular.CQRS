875686c6ad1df550b2a37c29fdcc328e2beeffc6
mminder <marvinminder@gmail.com>
2014-06-30 15:09:24 +0200

    [TENAC-27] fixed bug in expect-promises (undefined asserted object)

--- test/testUtils/expect-promises.js
+++ test/testUtils/expect-promises.js
@@ -12,44 +12,44 @@
  */
 
 function extendWithPromiseLogic(functionName) {
-   var originalFunctionName = functionName + 'original';
-   expect.Assertion.prototype[originalFunctionName] = expect.Assertion.prototype[functionName];
-   expect.Assertion.prototype[functionName] = function () {
-      // this.obj is the 'actual' result to test
+  var originalFunctionName = functionName + 'original';
+  expect.Assertion.prototype[originalFunctionName] = expect.Assertion.prototype[functionName];
+  expect.Assertion.prototype[functionName] = function () {
+    // this.obj is the 'actual' result to test
 
-      if (!this.obj.then) {
-         return this[originalFunctionName].apply(this, arguments);
-      }
+    if (!this.obj || !this.obj.then) {
+      return this[originalFunctionName].apply(this, arguments);
+    }
 
-      var assertionInstance = this,
-         assertionFunctionArguments = arguments;
-      this.obj.then(function (result) {
-         assertionInstance.obj = result;
-         assertionInstance[originalFunctionName].apply(assertionInstance, assertionFunctionArguments);
-      });
-      return this;
-   };
+    var assertionInstance = this,
+      assertionFunctionArguments = arguments;
+    this.obj.then(function (result) {
+      assertionInstance.obj = result;
+      assertionInstance[originalFunctionName].apply(assertionInstance, assertionFunctionArguments);
+    });
+    return this;
+  };
 }
 
 function extendWithRejectAssertion() {
-   expect.Assertion.prototype.toBeRejected = function () {
-      if (!this.obj.then) {
-         throw new Error('Actual object to test is not a promise! (' + this.obj + ')');
-      }
+  expect.Assertion.prototype.toBeRejected = function () {
+    if (!this.obj.then) {
+      throw new Error('Actual object to test is not a promise! (' + this.obj + ')');
+    }
 
-      this.obj.then(function (result) {
-         throw new Error('expected promise to be rejected. Instead resolved to ' + result);
-      }, function () {
-         // this is ok -> success
-      });
+    this.obj.then(function (result) {
+      throw new Error('expected promise to be rejected. Instead resolved to ' + result);
+    }, function () {
+      // this is ok -> success
+    });
 
-   };
+  };
 }
 
 Object.getOwnPropertyNames(expect.Assertion.prototype).forEach(function (propertyName) {
-   if (typeof expect.Assertion.prototype[propertyName] === 'function') {
-      extendWithPromiseLogic(propertyName);
-   }
+  if (typeof expect.Assertion.prototype[propertyName] === 'function') {
+    extendWithPromiseLogic(propertyName);
+  }
 });
 
 extendWithRejectAssertion();
