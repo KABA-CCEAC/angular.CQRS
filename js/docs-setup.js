b92c87f450826dedff4e8fb96b41d5b0ef2069f8
taibika <xeronimus@gmail.com>
2014-07-04 15:38:46 +0200

    [TENAC-21] introduce "sendCommand"-callbacks
    - same functionality as in backbone.cqrs
    - CQRS will produce a objectId for every sent command, you can register a
      callback that is invoked if an corresponding event (same commandId) is
      received

--- js/docs-setup.js
+++ js/docs-setup.js
@@ -10,7 +10,7 @@ NG_DOCS={
       "type": "object",
       "moduleName": "ngCQRS",
       "shortDescription": "Handles the configuration of the CQRS module.",
-      "keywords": "angular api configuration cqrs event events factory form function generate generates handles identifier incoming modify module ngcqrs object optional parameter parameters parse parser parserfunction pass provider query registers response responses returned setqueryparser seturlfactory string tourlgetparameterstring url urlfactoryfunction urls viewmodelname"
+      "keywords": "angular api comanndid commandid commandidfunction configuration cqrs event events extract extraction factory form function generate generates handles identifier incoming modify module ngcqrs object optional parameter parameters parse parser parserfunction parsing pass provider query received registers response responses returned setcommandidextractionfunction setqueryparser seturlfactory string tourlgetparameterstring url urlfactoryfunction urls viewmodelname"
     },
     {
       "section": "api",
@@ -19,7 +19,7 @@ NG_DOCS={
       "type": "service",
       "moduleName": "ngCQRS",
       "shortDescription": "Is used to send commands and define the specific channel over which messages will be sent.",
-      "keywords": "aggregatetype angular api application arrived backend call changeprofile channel command commandobject commands connect connection cqrs data define description emit event eventreceived events function io javascript listener long message messages mysocket ngcqrs object oncommand order pass payload polling profile received register registered result send sendcommand sends server service socket solution specific usage var websocket wire"
+      "keywords": "aggregatetype angular api application arrived backend call callback callbackfunction changeprofile channel command commandobject commands connect connection correspondant cqrs data define description emit event eventreceived events function invoked io javascript listener long message messages mysocket ngcqrs object oncommand optional order pass payload polling profile received register registered result returns send sendcommand sends server service socket solution specific usage var websocket wire"
     },
     {
       "section": "api",
@@ -32,12 +32,21 @@ NG_DOCS={
     },
     {
       "section": "api",
+      "id": "ngCQRS.service:ObjectId",
+      "shortName": "ObjectId",
+      "type": "service",
+      "moduleName": "ngCQRS",
+      "shortDescription": "Used to obtain a objectId that mimics how WCF serializes a object of type MongoDB.Bson.ObjectId",
+      "keywords": "api array bson byte character console converts format function https identifier increment inspired instance javascript js log machine mimics mongodb mymodule myservice ngcqrs object objectid objectidone objectidthree objectidtwo optional pid process representation returns serializes service standard string timestamp toarray tostring type unix usage var wcf"
+    },
+    {
+      "section": "api",
       "id": "ngCQRS.service:Store",
       "shortName": "Store",
       "type": "service",
       "moduleName": "ngCQRS",
       "shortDescription": "The store allows for querying modelViews and registering for subsequent events on that viewModelName.",
-      "keywords": "$scope allows angular api appropriate callback called controller cqrs createforcontroller denormalizationservice denormalized denormalizer events function handler identifier javascript modelviews ngcqrs object optional parameters pass passed persondetails profile provider query querying register registerdenormalizerfunction registerdenormalizerfunctions registering response service seturlfactory store storeservice subsequent toghether updates url urlfactory usage var viewmodel viewmodelname"
+      "keywords": "$scope allows angular api appropriate callback called controller cqrs createforcontroller denormalizationservice denormalized denormalizer events function handler identifier javascript modelviews ngcqrs object optional parameters pass passed persondetails profile provider query querying register registerdenormalizerfunctions registering response service seturlfactory store storeservice subsequent toghether updates url urlfactory usage var viewmodel viewmodelname"
     },
     {
       "section": "api",
